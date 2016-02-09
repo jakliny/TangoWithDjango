8eeac9cff9265da10d907f5bc3697b22a47c07af
Jaklin <jaki.yordanova@gmail.com>
2016-02-09 13:18:18 +0000

    Chapter 7

--- rango/views.py
+++ rango/views.py
@@ -11,7 +11,7 @@ def index(request):
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list,}
-
+    #
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
 
